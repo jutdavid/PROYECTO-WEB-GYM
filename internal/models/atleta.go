985c730c8050ae41ebe828885fd60d73e04c0682
jutdavid <jutdavid@users.noreply.github.com>
2026-06-09 23:55:30 -0500

    feat(models): add MetodologiaObjetivo and Peso fields

--- internal/models/atleta.go
+++ internal/models/atleta.go
@@ -1,6 +1,8 @@
 package models
 
 type Atleta struct {
-	ID     uint   `json:"id"`
-	Nombre string `json:"nombre"`
+	ID                  uint    `json:"id"`
+	Nombre              string  `json:"nombre"`
+	MetodologiaObjetivo string  `json:"metodologia_objetivo"`
+	Peso                float64 `json:"peso"`
 }
