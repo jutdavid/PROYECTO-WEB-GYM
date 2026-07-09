f94ad67c580e4f63c5d3f8a7ec873254689486a0
Israel-13-1 <davischa2008@gmail.com>
2026-07-09 10:18:27 -0500

    feat: crear modelo MetricaFisica

--- internal/models/metrica_fisica.go
+++ internal/models/metrica_fisica.go
@@ -2,12 +2,11 @@ package models
 
 import "time"
 
-// MetricaFisica representa el progreso corporal del atleta en el tiempo.
-// AtletaID referencia al Atleta evaluado.
 type MetricaFisica struct {
-	ID              int       `json:"id"`
+	ID              int       `json:"id" gorm:"primaryKey"`
 	AtletaID        int       `json:"atleta_id"`
+	FechaMedicion   time.Time `json:"fecha_medicion"`
 	PorcentajeGrasa float64   `json:"porcentaje_grasa"`
 	MasaMuscular    float64   `json:"masa_muscular"`
-	FechaMedicion   time.Time `json:"fecha_medicion"`
+	Atleta          *Atleta   `json:"atleta,omitempty" gorm:"foreignKey:AtletaID"`
 }
